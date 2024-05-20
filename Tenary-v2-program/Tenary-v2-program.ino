232d664a5734cc235bbd44374a4b4b0e3c485d0a
Ume <79046153+EarthGMS@users.noreply.github.com>
2024-05-20 18:12:40 +0700

    First push

---
+++ Tenary-v2-program/Tenary-v2-program.ino
@@ -0,0 +1,17 @@
+ /*Tenary 2.0
+Made by : Pongsapat Boonpong
+Purpose : To extend functions and applications from the Tenary 1.0
+Version : v2.0
+Finish date : TBD
+*/
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
