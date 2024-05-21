8126cde1c7249dd4b6f6a4af8772bc09e3860ad1
Ume <79046153+EarthGMS@users.noreply.github.com>
2024-05-21 20:17:57 +0700

    add pseudocode

--- Tenary-v2-program/Tenary-v2-program.ino
+++ Tenary-v2-program/Tenary-v2-program.ino
@@ -1,17 +1,46 @@
- /*Tenary 2.0
+/*Tenary 2.0
 Made by : Pongsapat Boonpong
 Purpose : To extend functions and applications from the Tenary 1.0
 Version : v2.0
 Finish date : TBD
 */
 
+// Modes
 
-void setup() {
-  
+// Data output
 
+// Indicators
+
+void setup()
+{
+    // Set pin mode for all inputs and outputs
+
+    // Set mode pins
 }
 
-void loop() {
-  
+void loop()
+{
+    // Turn off output pins
 
+    // Turn off LED indicator pin
+
+    // Read digital input from 2 input sets
+
+    // Read digital input from 4 mode inputs
+
+    // Convert binary input to decimal function
+
+    // Print value of set 1 and set 2
+
+    // Math operation function (set1, set2)
+
+    // Print math result
+
+    // Set up negative pins
+
+    // Output setup function
+
+    // Reset set1, set2 and math result
+
+    // delay for 2 secs and repeat
 }
